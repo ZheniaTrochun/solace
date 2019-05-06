d984ff6a309391a032bd3e0218549d57876affdd
Zhenia Trochun <zheniatrochun@gmail.com>
2019-05-06 22:06:46 +0300

    improved logging, made L2Table trait with in-memory implementation

---
+++ solace-server/src/main/scala/com/yevhenii/solace/l2/InMemoryL2.scala
@@ -0,0 +1,44 @@
+package com.yevhenii.solace.l2
+
+import java.util.concurrent.ConcurrentHashMap
+
+import com.typesafe.scalalogging.Logger
+import com.yevhenii.solace.messages.Messages.{EthernetMessage, Message}
+
+class InMemoryL2 extends L2Table {
+
+  import L2Table._
+
+  val logger = Logger("InMemoryL2")
+
+  val table: ConcurrentHashMap[String, ConcurrentHashMap[String, String]] = new ConcurrentHashMap[String, ConcurrentHashMap[String, String]]()
+
+  def learn(msg: Message, ethernetMessage: EthernetMessage, dpid: String): Unit = {
+    val dlSrc = ethernetMessage.shost
+    val dlDst = ethernetMessage.dhost
+    val inPort = msg.body.get.in_port.get
+
+    if (isBroadcast(dlSrc)) {
+      logger.warn("Source set to broadcast!")
+    } else {
+      table.computeIfAbsent(dpid, _ => new ConcurrentHashMap[String, String]())
+      val map = table.get(dpid)
+      if (map.containsKey(dlSrc)) {
+        val dst = map.get(dlSrc)
+        if (dst != inPort) {
+          logger.info(s"MAC has moved from $dst to $inPort")
+          //          map.put(dlSrc, inPort) // TODO investigate
+        } else {
+          logger.info("This entry was already learnt")
+        }
+      } else {
+        logger.info(s"Learned mac $dlSrc port: $inPort")
+        map.put(dlSrc, inPort)
+      }
+    }
+  }
+
+  def get(dpid: String): ConcurrentHashMap[String, String] = {
+    table.get(dpid)
+  }
+}
