1db7480caccf11f9e9b71a39988bb3e762969ee4
Zhenia Trochun <zheniatrochun@gmail.com>
2019-05-09 17:37:13 +0300

    tried akka tcp

--- solace-server/src/main/scala/com/yevhenii/solace/AkkaSocketServer.scala
+++ solace-server/src/main/scala/com/yevhenii/solace/AkkaSocketServer.scala
@@ -52,6 +52,8 @@ class EchoHandler(connection: ActorRef, remote: InetSocketAddress) extends Actor
   import Tcp._
   import EchoHandler._
 
+  val localAddress = new InetSocketAddress("0.0.0.0", 6633)
+
   // sign death pact: this actor terminates when connection breaks
   context.watch(connection)
 
@@ -61,8 +63,8 @@ class EchoHandler(connection: ActorRef, remote: InetSocketAddress) extends Actor
   //#writing
   def writing: Receive = {
     case Received(data) =>
-//      connection ! Write(data, Ack(currentOffset))
-      log.info(s"received data: [${data.utf8String}]")
+      connection ! Write(data, Connected(remote, localAddress))
+      log.info(s"received raw data: [$data]")
 //      buffer(data)
 
     case Ack(ack) =>
@@ -193,84 +195,3 @@ class EchoHandler(connection: ActorRef, remote: InetSocketAddress) extends Actor
 
   //#storage-omitted
 }
-//#echo-handler
-
-//#simple-echo-handler
-class SimpleEchoHandler(connection: ActorRef, remote: InetSocketAddress) extends Actor with ActorLogging {
-
-  import Tcp._
-
-  // sign death pact: this actor terminates when connection breaks
-  context.watch(connection)
-
-  case object Ack extends Event
-
-  def receive = {
-    case Received(data) =>
-      buffer(data)
-      connection ! Write(data, Ack)
-
-      context.become({
-        case Received(data) => buffer(data)
-        case Ack            => acknowledge()
-        case PeerClosed     => closing = true
-      }, discardOld = false)
-
-    case PeerClosed => context.stop(self)
-  }
-
-  //#storage-omitted
-  override def postStop(): Unit = {
-    log.info(s"transferred $transferred bytes from/to [$remote]")
-  }
-
-  var storage = Vector.empty[ByteString]
-  var stored = 0L
-  var transferred = 0L
-  var closing = false
-
-  val maxStored = 100000000L
-  val highWatermark = maxStored * 5 / 10
-  val lowWatermark = maxStored * 3 / 10
-  var suspended = false
-
-  //#simple-helpers
-  private def buffer(data: ByteString): Unit = {
-    storage :+= data
-    stored += data.size
-
-    if (stored > maxStored) {
-      log.warning(s"drop connection to [$remote] (buffer overrun)")
-      context.stop(self)
-
-    } else if (stored > highWatermark) {
-      log.debug(s"suspending reading")
-      connection ! SuspendReading
-      suspended = true
-    }
-  }
-
-  private def acknowledge(): Unit = {
-    require(storage.nonEmpty, "storage was empty")
-
-    val size = storage(0).size
-    stored -= size
-    transferred += size
-
-    storage = storage.drop(1)
-
-    if (suspended && stored < lowWatermark) {
-      log.debug("resuming reading")
-      connection ! ResumeReading
-      suspended = false
-    }
-
-    if (storage.isEmpty) {
-      if (closing) context.stop(self)
-      else context.unbecome()
-    } else connection ! Write(storage(0), Ack)
-  }
-  //#simple-helpers
-  //#storage-omitted
-}
-//#simple-echo-handler
