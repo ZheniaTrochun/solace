cfb00bc0ab14053d544106d5492affe34b5834a7
Zhenia Trochun <zheniatrochun@gmail.com>
2019-05-10 00:27:15 +0300

    upgraded protocol version

--- solace-formatter/src/deserializer.js
+++ solace-formatter/src/deserializer.js
@@ -4,33 +4,35 @@ var decode = require('./decoder.js')
 
 var switchStream = new oflib.Stream();
 
-module.exports = {
-    unpack: (data) => {
-        console.dir(data)
-        const msgs = switchStream.process(Buffer.from(data, 'utf-8'))
-        msgs.forEach(msg => {
-            if (msg.hasOwnProperty('message')) {
-                console.log("message parsed")
-                console.dir(msg)
-                const type = msg.message.header.type
-                // decode ethernet message
-                if (type == 'OFPT_PACKET_IN') {
-                    const packet = decode.decodeethernet(obj.message.body.data, 0)
-                    console.dir(packet)
-                    msg.message.decodedEthernet = packet
-                }
-            } else {
-                util.log('Error: Message is unparseable')
-                console.dir(data)
+function unpackData(data) {
+    console.dir(data)
+    const msgs = switchStream.process(Buffer.from(data, 'utf-8'))
+    msgs.forEach(msg => {
+        if (msg.hasOwnProperty('message')) {
+            console.log("message parsed")
+            console.dir(msg)
+            const type = msg.message.header.type
+            // decode ethernet message
+            if (type == 'OFPT_PACKET_IN') {
+                const packet = decode.decodeethernet(obj.message.body.data, 0)
+                console.dir(packet)
+                msg.message.decodedEthernet = packet
             }
-        })
-        console.dir(msgs)
-        if ((msgs.length == 1) && (msgs[0].hasOwnProperty('error'))) {
-            if (msgs[0].error.desc.includes('OFPT_HELLO message at offset 0 has invalid length (') && (data.length > 8)) {
-                console.log("trying to drop not needed data and retry")
-                return unpack(data.substring(0, 8))
-            }
+        } else {
+            util.log('Error: Message is unparseable')
+            console.dir(data)
         }
-        return msgs
+    })
+    console.dir(msgs)
+    if ((msgs.length == 1) && (msgs[0].hasOwnProperty('error'))) {
+        if (msgs[0].error.desc.includes('OFPT_HELLO message at offset 0 has invalid length (') && (data.length > 8)) {
+            console.log("trying to drop not needed data and retry")
+            return unpackData(data.slice(0, 8))
+        }
     }
+    return msgs
+}
+
+module.exports = {
+    unpack: unpackData
 }
