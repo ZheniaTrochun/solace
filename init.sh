cfd56f01663d4f2f4d496d9857133dffd2a1d48c
Yevhenii Trochun <yevhenii.trochun@observepoint.com>
2019-06-09 12:13:20 +0300

    created infrastructure with docker-compose

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+docker pull influxdb
+docker pull grafana/grafana
+docker pull redis
+docker pull telegraf
+docker pull hseeberger/scala-sbt
+
+mkdir -p /tmp/docker/grafana/data
+mkdir -p /tmp/docker/influxdb/data
---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker-compose up -d
+
+echo "Grafana: http://127.0.0.1:3000 - admin/admin"
+
+echo
+echo "Current database list"
+curl -G http://localhost:8086/query?pretty=true --data-urlencode "db=glances" --data-urlencode "q=SHOW DATABASES"
+
