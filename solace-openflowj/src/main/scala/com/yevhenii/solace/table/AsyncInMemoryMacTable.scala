cf8eca6e16fc5f08f5e4ccc52150db3cfb1bedad
Yevhenii Trochun <yevhenii.trochun@observepoint.com>
2019-06-15 18:35:09 +0300

    fixed datapath

--- solace-openflowj/src/main/scala/com/yevhenii/solace/table/AsyncInMemoryMacTable.scala
+++ solace-openflowj/src/main/scala/com/yevhenii/solace/table/AsyncInMemoryMacTable.scala
@@ -6,17 +6,16 @@ import org.projectfloodlight.openflow.types.DatapathId
 
 import scala.concurrent.{ExecutionContext, Future}
 
-// TODO make dpid implicit
 class AsyncInMemoryMacTable(implicit ec: ExecutionContext)
   extends MacTable[String, Short, Future] {
   private val table = new ConcurrentHashMap[String, Short]()
 
-  override def contains(key: String)(implicit dpid: DatapathId): Future[Boolean] = Future.successful(table.containsKey(key))
+  override def contains(key: String)(implicit dpid: DatapathId): Future[Boolean] = Future { table.containsKey(s"$dpid-$key") }
 
-  override def get(key: String)(implicit dpid: DatapathId): Future[Option[Short]] = Future {Option(table.get(key))}
+  override def get(key: String)(implicit dpid: DatapathId): Future[Option[Short]] = Future { Option(table.get(s"$dpid-$key")) }
 
   override def put(key: String, value: Short)(implicit dpid: DatapathId): Future[Boolean] = Future {
-    table.put(key, value)
+    table.put(s"$dpid-$key", value)
     true
   }
 }
